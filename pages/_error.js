8dbfa8dae2bb82345b7f85a5c7487c36d5ed43cc
AdinlewaAbraham <abrahamadinlewa@gmail.com>
2023-07-17 00:41:59 +0100

    prod fix at 2

--- pages/_error.js
+++ pages/_error.js
@@ -17,10 +17,4 @@ const ErrorPage = ({ statusCode }) => {
   );
 };
 
-export async function getServerSideProps({ res, err }) {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { props: { statusCode } };
-}
-
-
 export default ErrorPage;
